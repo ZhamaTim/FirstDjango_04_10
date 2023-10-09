f46dbc41c36d5a76d735493bba50352c3d412633
Timur Zhamaletdinov <tim.zhamal@mail.ru>
2023-10-09 17:20:52 +0300

    + descriptions

--- MainApp/models.py
+++ MainApp/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     name = models.CharField ( max_length = 100)
     brand = models.CharField (max_length=100)
     count = models.PositiveBigIntegerField()
+    description = models.TextField(max_length=200, default='Базовое описание') 
 
     def __repr__(self) :
         return f"Item({self.name},{self.brand},{self.count}"
